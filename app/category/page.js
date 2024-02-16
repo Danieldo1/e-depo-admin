5ce1fe7bda0138f6b1a93823fa4fc707758b8af2
Dan <daniel.speranskiy@live.com>
2024-02-16 20:21:29 +0400

    adding delete categories

--- app/category/page.js
+++ app/category/page.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from "react";
 import Layout from "@/components/Layout";
 import Link from "next/link";
 import { Trash2 } from "lucide-react";
+import { withSwal } from "react-sweetalert2";
 
-const CategoryPage = () => {
+function CategoryPage({ swal }) {
   const [name, setName] = useState("");
   const [categories, setCategories] = useState([]);
   const [parent, setParent] = useState("");
@@ -26,36 +27,33 @@ const CategoryPage = () => {
   const saveCategory = async (e) => {
     e.preventDefault();
     if (editing) {
-        
-        await fetch(`/api/category`, {
-          method: "PUT",
-          body: JSON.stringify({ _id: editing._id, name, parent }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }).then((response) => {
-          response.json().then((data) => {
-            console.log(data);
-            setEditing(null);
-            fetchCategories();
-            setName("");
-            setParent("");
-          });
+      await fetch(`/api/category`, {
+        method: "PUT",
+        body: JSON.stringify({ _id: editing._id, name, parent }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setEditing(null);
+          fetchCategories();
+          setName("");
+          setParent("");
         });
-        
+      });
     } else {
-
-        await fetch("/api/category", {
-          method: "POST",
-          body: JSON.stringify({ name, parent }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }).then((response) => {
-          response.json().then((data) => {
-            console.log(data);
-          });
+      await fetch("/api/category", {
+        method: "POST",
+        body: JSON.stringify({ name, parent }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => {
+        response.json().then((data) => {
+          console.log(data);
         });
+      });
     }
     setName("");
     fetchCategories();
@@ -63,13 +61,38 @@ const CategoryPage = () => {
 
   const editCategory = async (category) => {
     setEditing(category);
-        setName(category.name);
-        setParent(category.parent?._id || category);
+    setName(category.name);
+    setParent(category.parent?._id || category);
+  };
+
+  const deleteCategory = async (category) => {
+    await fetch(`/api/category`, {
+      method: "DELETE",
+      body: JSON.stringify({ _id: category._id }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+        setEditing(null);
+        setName("");
+        setParent("");
+        fetchCategories();
+      });
+    });
   }
   return (
     <Layout>
       <h1 className="heading">Category Page</h1>
-      <label htmlFor="" className="flex flex-row mb-2">{editing ? "Edit" : "Create"} Category {editing ? (<p className="font-bold text-base ml-3">{editing.name}</p>) : ""}</label>
+      <label htmlFor="" className="flex flex-row mb-2">
+        {editing ? "Edit" : "Create"} Category{" "}
+        {editing ? (
+          <p className="font-bold text-base ml-3">{editing.name}</p>
+        ) : (
+          ""
+        )}
+      </label>
       <form onSubmit={saveCategory} className="flex gap-2">
         <input
           value={name}
@@ -87,11 +110,13 @@ const CategoryPage = () => {
             Main Category
           </option>
           {categories.length > 0 &&
-            categories.filter((category) => !category.parent).map((category) => (
-              <option key={category._id} value={category._id}>
-                {category.name}
-              </option>
-            ))}
+            categories
+              .filter((category) => !category.parent)
+              .map((category) => (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              ))}
         </select>
         <button className="btn-save py-1" type="submit">
           {editing ? "Update" : "Save"}
@@ -102,22 +127,40 @@ const CategoryPage = () => {
         {categories.length > 0 &&
           categories.map((category) => (
             <div
-             onClick={() => editCategory(category)}
+              onClick={() => editCategory(category)}
               key={category._id}
               className="cursor-pointer"
             >
               <div className="flex my-2 gap-4 p-2 bg-blue-50 hover:bg-blue-100 justify-between items-center rounded-md">
                 <div>
                   <h1 className="text-xl font-bold">{category.name}</h1>
-                  <p className="text-sm px-1 bg-blue-500 text-white rounded-full font-bold text-center">{category?.parent?.name}</p>
+                  <p className="text-sm px-1 bg-blue-500 text-white rounded-full font-bold text-center">
+                    {category?.parent?.name}
+                  </p>
                 </div>
                 <div className="flex gap-5 items-center">
-                  <Link
-                    href={`/products/delete/${category._id}`}
+                  <div
+                    onClick={() => {
+                      swal
+                        .fire({
+                          title: "Are you sure?",
+                          text: "You won't be able to revert this!",
+                          icon: "warning",
+                          showCancelButton: true,
+                          confirmButtonColor: "#3085d6",
+                          cancelButtonColor: "#d33",
+                          confirmButtonText: "Yes, delete it!",
+                        })
+                        .then((result) => {
+                          if (result.isConfirmed) {
+                            deleteCategory(category);
+                          }
+                        });
+                    }}
                     className="bg-red-500 text-white p-2 rounded-lg items-center flex gap-2 hover:bg-red-600"
                   >
                     <Trash2 />
-                  </Link>
+                  </div>
                 </div>
               </div>
             </div>
@@ -125,6 +168,9 @@ const CategoryPage = () => {
       </div>
     </Layout>
   );
-};
+}
 
-export default CategoryPage;
+export default withSwal(({ swal }, ref) => (
+<CategoryPage swal={swal} />
+
+));
